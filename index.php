045aa6c1da6803896a1edb1e82611a5df0d73375
gytislaukaitisi <63237716+gytislaukaitisi@users.noreply.github.com>
2020-07-21 11:28:35 +0300

    begining

--- index.php
+++ index.php
@@ -3,20 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>File Manager</title>
+    <link rel="stylesheet" href= "./css/main.css">
+    <title>Document</title>
 </head>
 <body>
+   
 <?php
 function readFolderFiles($dir) {
-    $files = scandir("C:/Program Files/Ampps/www/");
-
+    $files = scandir($dir);
+    
     echo '<ul>';
     foreach ($files as $file) {
         if (substr($file, 0, 1) != '.') {
             echo '<li>';
             if (is_dir($dir . '/' . $file)) {
-                echo $file;
-                readFolderFiles($dir . '/' . $files);
+            echo "<li><img src='./images/folder.png' class = 'folder'> ".$file;
+                readFolderFiles($dir . '/' . $file);
             } else 
             echo '<a href ="' . $dir .'/' . $file . '">' .$file . '</a>';
             echo '</li>';
@@ -24,7 +26,7 @@ function readFolderFiles($dir) {
     }
     echo '</ul>';
 }
-     readFolderFiles($dir);
+readFolderFiles("C:/Program Files/Ampps/www/");
 ?>   
 </body>
 </html>
