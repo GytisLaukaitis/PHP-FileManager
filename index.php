0308709dabd9d29189b56ed33a8e51b11ea7b0e6
gytislaukaitisi <63237716+gytislaukaitisi@users.noreply.github.com>
2020-07-22 00:37:17 +0300

    php yra sudas

--- index.php
+++ index.php
@@ -7,26 +7,22 @@
     <title>Document</title>
 </head>
 <body>
-<form action="index.php" method="get">
-<input type="hidden" name="path" value="<?php print($_GET['path']) ?>" /> 
-</form>
-   
 <?php
-$dir = '..' . $_GET['path'];
-function readFolderFiles($dir) {
-    $files = scandir($dir);
-    
-    foreach ($files as $file) {
-        if (substr($file, 0, 1) != '.') {
-            if (is_dir($dir . '/' . $file)) {
-                echo '<a href ="' . $dir .'/' . $file . '">'  .$file . "</ul><img src='./images/folder.png' class = 'folder'>" . '</a>';
-                readFolderFiles($dir . '/' . $file);
-            } else 
-            echo '<a href ="' . '">';
-        }
-    }
-}
-readFolderFiles($dir);
+ $path = '../' . $_GET["path"];
+ $files = scandir($path);
+
+ echo('<tr>');
+ foreach ($files as $file){
+     if ($file != ".." and $file != ".") {
+         echo("<td><img src='./images/folder.png' class = 'folder'>" . (is_dir($path . $file) 
+                     ? '<a href="' . (isset($_GET['path']) 
+                             ? $_SERVER['REQUEST_URI'] . $file . '/' 
+                             : $_SERVER['REQUEST_URI'] . '?path=' . $file . '/') . '">' . $file . '</a>'
+                     : $file)
+             . "</td>");
+     }
+ }
+ echo('<tr>');
 ?>  
  
 </body>
