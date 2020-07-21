22ead435b210b2584b55ff74522df8ab68fcd2d1
gytislaukaitisi <63237716+gytislaukaitisi@users.noreply.github.com>
2020-07-21 10:13:26 +0300

    nieko gero

--- index.php
+++ index.php
@@ -3,9 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Git</title>
+    <title>File Manager</title>
 </head>
 <body>
-    
+<?php
+function readFolderFiles($dir) {
+    $files = scandir("C:/Program Files/Ampps/www/");
+
+    echo '<ul>';
+    foreach ($files as $file) {
+        if (substr($file, 0, 1) != '.') {
+            echo '<li>';
+            if (is_dir($dir . '/' . $file)) {
+                echo $file;
+                readFolderFiles($dir . '/' . $files);
+            } else 
+            echo '<a href ="' . $dir .'/' . $file . '">' .$file . '</a>';
+            echo '</li>';
+        }
+    }
+    echo '</ul>';
+}
+     readFolderFiles($dir);
+?>   
 </body>
 </html>
