f82576413d27f8ec69c03bc9c4cec2d39819551e
gytislaukaitisi <63237716+gytislaukaitisi@users.noreply.github.com>
2020-07-22 11:44:02 +0300

    ne mano

--- index.php
+++ index.php
@@ -8,21 +8,36 @@
 </head>
 <body>
 <?php
- $path = '../' . $_GET["path"];
- $files = scandir($path);
-
- echo('<tr>');
- foreach ($files as $file){
-     if ($file != ".." and $file != ".") {
-         echo("<td><img src='./images/folder.png' class = 'folder'>" . (is_dir($path . $file) 
-                     ? '<a href="' . (isset($_GET['path']) 
-                             ? $_SERVER['REQUEST_URI'] . $file . '/' 
-                             : $_SERVER['REQUEST_URI'] . '?path=' . $file . '/') . '">' . $file . '</a>'
-                     : $file)
-             . "</td>");
-     }
- }
- echo('<tr>');
+    $path = '../' . $_GET["path"];
+    $files_and_dirs = scandir($path);
+    print('<table><th>Type</th><th>Name</th><th>Actions</th>');
+    foreach ($files_and_dirs as $fnd){
+        if ($fnd != ".." and $fnd != ".") {
+            print('<tr>');
+            print('<td>' . (is_dir($path . $fnd) ? "Directory" : "File") . '</td>');
+            print('<td>' . (is_dir($path . $fnd) 
+                        ? '<a href="' . (isset($_GET['path']) 
+                                ? $_SERVER['REQUEST_URI'] . $fnd . '/' 
+                                : $_SERVER['REQUEST_URI'] . '?path=' . $fnd . '/') . '">' . $fnd . '</a>'
+                        : $fnd)
+                . '</td>');
+            print('<td>'
+                . (is_dir($path . $fnd) 
+                    ? ''
+                    : '<form style="display: inline-block" action="" method="post">
+                        <input type="hidden" name="delete" value=' . str_replace(' ', '&nbsp;', $fnd) . '>
+                        <input type="submit" value="Delete">
+                       </form>
+                       <form style="display: inline-block" action="" method="post">
+                        <input type="hidden" name="download" value=' . str_replace(' ', '&nbsp;', $fnd) . '>
+                        <input type="submit" value="Download">
+                       </form>'
+                ) 
+                . "</form></td>");
+            print('</tr>');
+        }
+    }
+    print("</table>");
 ?>  
  
 </body>
